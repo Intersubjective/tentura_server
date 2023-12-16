8da8e7566d37c66811cdc1276b11ec737013a551
V.G. Bulavintsev <golem.md@gmail.com>
2023-12-16 20:26:33 +0100

    Remove Meritrank-service copy

--- meritrank/main.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-from meritrank_service.asgi import create_meritrank_app
-
-uvicorn.run(create_meritrank_app(), host="0.0.0.0", port=8000)
--- meritrank/tests/test_restore_edges_data.py
+++
@@ -1,6 +0,0 @@
-from meritrank_service.postgres_edges_provider import get_edges_data
-
-
-def test_get_data_from_db():
-    url = "postgres://postgres:12345678@localhost:54321/postgres"
-    get_edges_data(url)
