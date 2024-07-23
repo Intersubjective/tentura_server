86389e78c65c926937934ea13aea5cff1a2e0a29
Postnikov Aleksander <alexandrim0@gmail.com>
2024-07-24 00:42:12 +0300

    - profile preview

--- openresty/app/preview.lua
+++ openresty/app/preview.lua
@@ -1,55 +1,98 @@
-local template = require 'resty.template.safe'
+local ngx = ngx
 local cjson = require 'cjson'
 local gql = require 'app.gql'
-local ngx = ngx
+local template = require 'resty.template'
 
-local CT_JSON = 'application/json'
+local CT_HTML = 'text/html; charset=utf-8'
 
-local HEADER_VIEW = template.new[[
-<head>
-  <title>Tentura</title>
-  <base href="https://{{server_name}}/">
-  <link rel="canonical" href="https://{{server_name}}/">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_24dp.png" sizes="24x24">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_32dp.png" sizes="32x32">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_36dp.png" sizes="36x36">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_48dp.png" sizes="48x48">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_64dp.png" sizes="64x64">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_96dp.png" sizes="96x96">
-  <link rel="shortcut icon" href="//{{server_name}}/static/logo/web_512dp.png" sizes="512x512">
-  <meta name="robots" content="noindex">
-  <meta name="referrer" content="origin-when-cross-origin">
-  <meta name="description" content="Social network for communities.">
-  <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1 user-scalable=no">
-  <meta property="og:url" content="https://{{server_name}}"/>
-  <meta property="og:type" content="website"/>
-  <meta property="og:title" content="Tentura"/>
-  <meta property="og:description" content="Social network for communities."/>
-  <meta property="og:image" content="https://{{server_name}}/static/logo/web_96dp.png"/>
-</head>
-]]
-
-local PROFILE_VIEW = template.new[[
+local HEADER = [[
 <!doctype html>
 <html lang="en" dir="ltr">
-  {*header*}
-	<body style="height:100%;overflow:hidden;-webkit-font-smoothing:antialiased;color:rgba(0,0,0,0.87);font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif;font-weight:400;margin:0;-webkit-text-size-adjust:100%;-webkit-text-size-adjust:100%;text-size-adjust:100%;-webkit-user-select:none">
-		<div class="root">
-			<div style="padding:32px 64px">
-				<img src="https://{{server_name}}/static/logo/web_96dp.png" alt="Tentura" style="border:none">
-			</div>
-			<div style="color:#5f6368;font-size:20px;font-weight:500;padding-left:16px">
-				Social network for communities.
-			</div>
-		</div>
-	</body>
+  <head>
+    <title>Tentura</title>
+    <link rel="stylesheet" href="/static/css/preview.css"/>
+    <link rel="shortcut icon" href="/static/logo/web_24dp.png" sizes="24x24">
+    <link rel="shortcut icon" href="/static/logo/web_32dp.png" sizes="32x32">
+    <link rel="shortcut icon" href="/static/logo/web_36dp.png" sizes="36x36">
+    <link rel="shortcut icon" href="/static/logo/web_48dp.png" sizes="48x48">
+    <link rel="shortcut icon" href="/static/logo/web_64dp.png" sizes="64x64">
+    <link rel="shortcut icon" href="/static/logo/web_96dp.png" sizes="96x96">
+    <link rel="shortcut icon" href="/static/logo/web_512dp.png" sizes="512x512">
+    <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1 user-scalable=no">
+    <meta name="referrer" content="origin-when-cross-origin">
+    <meta name="robots" content="noindex">
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="Tentura"/>
+    <meta property="og:title" content="{{title}}"/>
+    <meta property="og:description" content="{{description}}"/>
+    <meta property="og:url" content="https://{{server_name}}/{*content_uri*}"/>
+    <meta property="og:image" content="https://{{server_name}}/{*image_path*}.jpg"/>
+  </head>
+  <body>
+]]
+
+local PROFILE = [[
+    <div class="profile-card">
+      <div class="profile-image">
+        <img src="{*image_path*}.jpg" alt="Profile Image">
+      </div>
+      <div class="profile-info">
+        <div class="profile-name">{{title}}</div>
+        <div class="profile-description">{{description}}</div>
+      </div>
+    </div>
+  </body>
 </html>
 ]]
 
+local BEACON = [[
+    <div class="post-card">
+      <div class="header">
+          <img src="https://i.imgur.com/utMtNp7.png" alt="Author Avatar" class="avatar">
+          <div class="header-info">
+              <div>Josh Sunderland</div>
+              <div class="date">15 Jan</div>
+          </div>
+      </div>
+      <div class="post-image">
+          <img src="https://i.imgur.com/XS7HiEi.png" alt="Starry Sky">
+      </div>
+      <div class="post-content">
+          <h1>Staying Ahead of the Curve</h1>
+          <p>As technology continues to advance at an unprecedented pace, it's becoming increasingly important for individuals and businesses to stay up-to-date with the latest trends and developments in the industry.</p>
+      </div>
+      <div class="footer">
+          <div>Copenhagen, Denmark</div>
+          <div>👍 10</div>
+      </div>
+
+      <!-- Comment Section -->
+      <div class="comment-card">
+          <div class="comment-header">
+              <img src="https://i.imgur.com/4LC76FR.png" alt="Commenter Avatar" class="avatar">
+              <div class="header-info">
+                  <div>Alyssa Stewart</div>
+                  <div class="time">7m</div>
+              </div>
+          </div>
+          <div class="comment-content">
+              <p>One of the best ways to stay up-to-date with the latest trends and developments in the tech industry is to join online forums and communities.</p>
+          </div>
+          <div class="comment-footer">
+              <div>👍 10</div>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
+]]
+
+local PROFILE_VIEW = HEADER..PROFILE
+local BEACON_VIEW = HEADER..BEACON
+
 local QUERY_PROFILE_FETCH = [[
 query ($id: String!) {
-  user(where: {id: {_eq: $id}}) {
-    id
+  user_by_pk(id: $id) {
     title
     description
     has_picture
@@ -57,33 +100,43 @@ query ($id: String!) {
 }
 ]]
 
+
 ---@return nil
 local function profile_view()
+  local id = ngx.req.get_uri_args().id
   local data, errors = gql.query(
     QUERY_PROFILE_FETCH,
-    { id = ngx.req.get_uri_args().id })
-  if errors then
-    ngx.log(ngx.INFO, cjson.encode(errors))
+    { id = id }
+  )
+  if errors or data == nil then
+    local err = cjson.encode(errors or 'Unknown error')
+    ngx.var.xlog = err
+    ngx.log(ngx.INFO, err)
     return ngx.exit(ngx.HTTP_BAD_REQUEST)
   end
 
-  ngx.header.content_type = CT_JSON
-  ngx.say(data)
+  local user = data.user_by_pk
+  local server_name = ngx.var.server_name
+  ngx.header.content_type = CT_HTML
+  ngx.say(template.process_string(
+    PROFILE_VIEW,
+    {
+      title = user.title,
+      description = user.description,
+      server_name = server_name == '_'
+        and 'localhost'
+        or server_name,
+      image_path = user.has_picture
+        and '/images/'..id
+        or '/static/img/avatar-placeholder',
+      content_uri = 'beacon/view?id='..id,
+    }
+  ))
   return ngx.exit(ngx.OK)
-end
-
-
----@param server_name string
----@return nil
-local function init(server_name)
-  HEADER_VIEW.server_name = server_name
-  PROFILE_VIEW.server_name = server_name
-  PROFILE_VIEW.header = HEADER_VIEW:render()
 end
 
 
 return {
   _VERSION = '0.0.1',
-  init = init,
   profile_view = profile_view,
 }
